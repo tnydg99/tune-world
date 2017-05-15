85a2d69d1d6e2475635cf78d151c5766344f3402
User <user@users-mac-mini-2.local>
2017-05-15 17:36:11 -0400

    Initial Commit

---
+++ TuneWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuneWorld
+//
+//  Created by User on 5/15/17.
+//  Copyright © 2017 Austin Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
