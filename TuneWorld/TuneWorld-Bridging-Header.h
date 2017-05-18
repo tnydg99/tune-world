866fabb125c26369ec34ccec1949897f233d23e9
User <user@users-mac-mini-2.local>
2017-05-18 17:28:33 -0400

    initial commit

---
+++ TuneWorld/TuneWorld-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  TuneWorldBridgingHeader.h
+//  TuneWorld
+//
+//  Created by User on 5/18/17.
+//  Copyright © 2017 Austin Tucker. All rights reserved.
+//
+
+#ifndef TuneWorldBridgingHeader_h
+#define TuneWorldBridgingHeader_h
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SafariServices/SafariServices.h>
+
+#endif /* TuneWorldBridgingHeader_h */
